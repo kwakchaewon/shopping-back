4ea665ac4dccc6b4ee7cacce14ac55bee4b184a2
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2024-02-15 19:01:57 +0900

    data.sql 을 통한 샘플 데이터 생성

--- src/main/java/com/gallery/backend/entity/Item.java
+++ src/main/java/com/gallery/backend/entity/Item.java
@@ -7,11 +7,20 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-@Table(name="items")
+@Table(name="item")
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(length = 50, nullable = false)
     private String name;
+
+    @Column
+    private String imgPath;
+
+    @Column
+    private int price;
+
+    @Column
+    private int discountPer;
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into item (name, img_path, price, discount_per) values ('곽채원', 'img/chaewon.jpg', '10000000', 5);
+insert into item (name, img_path, price, discount_per) values ('정아림', 'img/logo.png', '20000000', 10);
+insert into item (name, img_path, price, discount_per) values ('유승원', 'img/screenshot.png', '30000000', 15);
